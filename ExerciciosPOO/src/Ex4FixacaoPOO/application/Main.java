d0659ed09965ecf5fd389ff8e27546d651383053
caduxdev <cadux2004dev@gmail.com>
2026-05-26 15:10:16 -0300

    Correções do Exercício da Conta de Banco

--- ExerciciosPOO/src/Ex4FixacaoPOO/application/Main.java
+++ ExerciciosPOO/src/Ex4FixacaoPOO/application/Main.java
@@ -6,50 +6,39 @@ public class Main {
 
     void main(){
         Scanner sc = new Scanner(System.in);
+        Account account;
 
         System.out.print("Enter account number: ");
         int account_number = sc.nextInt();
         System.out.print("Enter account holder: ");
         sc.nextLine();
         String account_holderName = sc.nextLine();
-        System.out.print("Is there na initial deposit (y/n)?");
+        System.out.print("Is there na initial deposit (yes/no)?");
         String depositOrNot = sc.nextLine();
-        if (depositOrNot.equals("y")){
-            System.out.print("Enter initial deposit value: ");
+
+        if (depositOrNot.equals("yes")){
+            System.out.print("\nEnter initial deposit value: ");
             double initalDeposit = sc.nextDouble();
-            Account account;
             account = new Account(account_number, account_holderName, initalDeposit);
-            System.out.println(account);
-
-            System.out.print("Enter a deposit value: ");
-            double deposit = sc.nextDouble();
-            account.accountDeposit(deposit);
-            System.out.println("Update Account data:");
-            System.out.println(account);
-
-            System.out.print("Enter a withdraw value: ");
-            double withdraw = sc.nextDouble();
-            account.accountWithdraw(withdraw);
-            System.out.println("Update Account data:");
             System.out.println(account);
         }
         else{
-            Account account;
             account = new Account(account_number, account_holderName);
             System.out.println(account);
-
-            System.out.print("Enter a deposit value: ");
-            double deposit = sc.nextDouble();
-            account.accountDeposit(deposit);
-            System.out.println("Update Account data:");
-            System.out.println(account);
-
-            System.out.print("Enter a withdraw value: ");
-            double withdraw = sc.nextDouble();
-            account.accountWithdraw(withdraw);
-            System.out.println("Update Account data:");
-            System.out.println(account);
         }
+
+        System.out.print("\nEnter a deposit value: ");
+        double deposit = sc.nextDouble();
+        account.accountDeposit(deposit);
+        System.out.print("\nUpdate Account data:");
+        System.out.println(account);
+
+        System.out.print("\nEnter a withdraw value: ");
+        double withdraw = sc.nextDouble();
+        account.accountWithdraw(withdraw);
+        System.out.print("\nUpdate Account data:");
+        System.out.println(account);
+
         sc.close();
     }
 
